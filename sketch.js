efbff62cb17522a1d15757990367dabe5da3241e
Dabeet660 <66428574+Dabeet660@users.noreply.github.com>
2020-09-21 14:55:17 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -18,7 +18,7 @@ function setup() {
 
 	packageSprite=createSprite(width/2, 80, 10,10);
 	packageSprite.addImage(packageIMG)
-	packageSprite.scale=0.4
+	packageSprite.scale=0.2
 
 	helicopterSprite=createSprite(width/2, 200, 10,10);
 	helicopterSprite.addImage(helicopterIMG)
@@ -31,7 +31,7 @@ function setup() {
 	engine = Engine.create();
 	world = engine.world;
 
-	packageBody = Bodies.circle(width/2 , 200 , 5 , {restitution:1.5, isStatic:true});
+	packageBody = Bodies.circle(width/2 , 200 , 5 , {restitution:0.74, isStatic:true});
 	World.add(world, packageBody);
 	
 
